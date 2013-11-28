064864e27715929d7a4deb683ad0dd7af9e75619
Rainer Kuhn <ticktricktrack@gmail.com>
2013-11-28 16:21:29 +0000

    basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HfApiTest::Application.config.secret_key_base = '11ebca0eb986d6cb413b1060af82d846e8a968f462087aab26c885b18dbd11a92b4a8d88665bbc70119ecd276c3d3e9d87ae30ce7801a9258b68dd30bea5972a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HfApiTest::Application.config.session_store :cookie_store, key: '_hf_api_test_session'
